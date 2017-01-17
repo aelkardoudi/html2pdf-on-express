c836bdd440b36699be5171b093f45f3b9cf62805
aelkardoudi <elkardoudi.abdelilah@gmail.com>
2017-01-17 12:20:08 +0100

    create apis

---
+++ routes/api.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var wkhtmltopdf = require('wkhtmltopdf');
+var multer = require('multer');
+var fileUpload = multer({
+  dest: './tmp/'
+}).single('file');
+var fs = require("fs");
+
+
+router.post('/html2pdf', fileUpload, function(req, res) {
+  console.log(req.file);
+  if (req.file.mimetype == "text/html") {
+    res.set('Content-Type', 'application/pdf');
+    res.attachment(req.file.originalname + '.pdf');
+    wkhtmltopdf(fs.createReadStream(req.file.path), {
+      pageSize: 'letter',
+      output: "test.pdf"
+    }, function(err, stream) {
+      if (err) {
+        console.error(err);
+      }
+      console.log("done");
+    }).pipe(res);
+  } else {
+    res.status(400).send("400 : Bad request or a bad type !");
+  }
+
+});
+
+router.post('/url2pdf', function(req, res) {
+  res.set('Content-Type', 'application/pdf');
+  wkhtmltopdf(req.body.url, {
+      pageSize: 'letter'
+    }, function(err, stream) {
+      if (err) {
+        console.error(err);
+      }
+    })
+    .pipe(res);
+});
+
+module.exports = router;
